d4f0fd9044c0533d1278cc341fc7b124dbf213bf
Timothy Washington <twashing@interruptsoftware.com>
2015-03-08 14:01:31 +0000

    -- cheatsheet link for Markdown mode

--- packages/sunra-markdown.el
+++ packages/sunra-markdown.el
@@ -1,4 +1,6 @@
-;; Docs: http://jblevins.org/projects/markdown-mode/
+;; Docs 
+;;   http://jblevins.org/projects/markdown-mode/
+;;   http://www.cheatography.com/xaon/cheat-sheets/emacs-markdown-mode/
 (use-package markdown-mode :ensure t)
 
 (provide 'sunra-markdown)
