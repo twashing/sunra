da1e30d25cd7d985125d0e98940cf1ebf6eb8a64
Timothy Washington <twashing@interruptsoftware.com>
2015-04-10 03:43:10 +0000

    -- adding function and config for 'copy-sexp-at-point

--- packages/core/sunra-navigation.el
+++ packages/core/sunra-navigation.el
@@ -1,10 +1,14 @@
 
+(defun copy-sexp-at-point ()
+    (interactive)
+      (kill-new (thing-at-point 'sexp)))
+
 (fset 'buf-move-up "\C-u10\C-p")
 (fset 'buf-move-down "\C-u10\C-n")
 
 (global-set-key (kbd "M-U") 'buf-move-up)
 (global-set-key (kbd "M-D") 'buf-move-down)
-
 (global-set-key (kbd "C-d") 'sp-kill-sexp)
+(global-set-key (kbd "C-M-k") 'copy-sexp-at-point)
 
 (provide 'sunra-navigation)
