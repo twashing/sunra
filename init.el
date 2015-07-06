3ce07edf6c2edfc428ef8d3de101cd550bce608b
Timothy Washington <tim.washington@nike.com>
2015-07-06 14:07:27 -0700

    -- git config seems to have changed in use-package

--- init.el
+++ init.el
@@ -12,7 +12,11 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(require 'use-package)
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
 
 
 (defmacro use-packages (&rest args)
@@ -82,4 +86,3 @@
 
 ;; Eval userinit
 (load "~/.sunra.d/init.el")
-
