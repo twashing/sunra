edc8aeb4f6a4220a9dfb77d8ca6bc13ea5f5556d
Timothy Washington <twashing@interruptsoftware.com>
2016-01-01 02:48:20 +0000

    -- first pass at adding hydra and accompanying configs

--- packages/core/sunra-multiplecursors.el
+++ packages/core/sunra-multiplecursors.el
@@ -1,4 +1,24 @@
 (use-package multiple-cursors
-  :defer t)
+  :defer t
+  :config (global-set-key (kbd "C-c M")
+			  (defhydra multiple-cursors-hydra (:hint nil)
+			    "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+                           "
+  ("l" mc/edit-lines :exit t)
+  ("a" mc/mark-all-like-this :exit t)
+  ("n" mc/mark-next-like-this)
+  ("N" mc/skip-to-next-like-this)
+  ("M-n" mc/unmark-next-like-this)
+  ("p" mc/mark-previous-like-this)
+  ("P" mc/skip-to-previous-like-this)
+  ("M-p" mc/unmark-previous-like-this)
+  ("r" mc/mark-all-in-region-regexp :exit t)
+  ("q" nil))))
 
 (provide 'sunra-multiplecursors)
--- packages/core/sunra-yasnippet.el
+++ packages/core/sunra-yasnippet.el
@@ -5,6 +5,29 @@
   :config (progn
 	    (yas-global-mode 1)
 	    (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets")
-	    (yas-load-directory "~/.emacs.d/snippets")))
+	    (yas-load-directory "~/.emacs.d/snippets")
+
+	    (global-set-key (kbd "C-c Y")
+			    (defhydra hydra-yasnippet (:color blue :hint nil)
+			        "
+              ^YASnippets^
+--------------------------------------------
+  Modes:    Load/Visit:    Actions:
+
+ _g_lobal  _d_irectory    _i_nsert
+ _m_inor   _f_ile         _t_ryout
+ _e_xtra   _l_ist         _n_ew
+         _a_ll
+"
+				("d" yas-load-directory)
+				("e" yas-activate-extra-mode)
+				("i" yas-insert-snippet)
+				("f" yas-visit-snippet-file :color blue)
+				("n" yas-new-snippet)
+				("t" yas-tryout-snippet)
+				("l" yas-describe-tables)
+				("g" yas/global-mode)
+				("m" yas/minor-mode)
+				  ("a" yas-reload-all)))))
 
 (provide 'sunra-yasnippet)
