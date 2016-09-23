945bb3a28dc19bb939f9e63c830ade8d5c896181
Timothy Washington <twashing@interruptsoftware.com>
2016-09-23 11:05:27 -0700

    Replacing haskell-mode with intero Better color highlights on helm selection

--- packages/lang/sunra-haskell.el
+++ packages/lang/sunra-haskell.el
@@ -1,27 +1,31 @@
-(use-package haskell-mode
-  :config
-  (progn
-    (require 'haskell-interactive-mode)
-    (require 'haskell-process)
-    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+;; (use-package haskell-mode
+;;   :config
+;;   (progn
+;;     (require 'haskell-interactive-mode)
+;;     (require 'haskell-process)
+;;     (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;     (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+;; 
+;;     ;; (autoload 'ghc-init "ghc" nil t)
+;;     ;; (autoload 'ghc-debug "ghc" nil t)
+;;     ;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+;;     ))
+;; 
+;; (use-package shm
+;;   :init (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+;;   :config
+;;   (progn
+;;     (when (require 'shm-case-split nil 'noerror)
+;;       ;;TODO: Find some better bindings for case-splits
+;;       (define-key shm-map (kbd "C-c S") 'shm/case-split)
+;;       (define-key shm-map (kbd "C-c C-s") 'shm/do-case-split))
+;; 
+;;     (define-key shm-map (kbd "C-j") nil)
+;;     (define-key shm-map (kbd "C-k") nil)))
 
-    ;; (autoload 'ghc-init "ghc" nil t)
-    ;; (autoload 'ghc-debug "ghc" nil t)
-    ;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-    ))
-
-(use-package shm
-  :init (add-hook 'haskell-mode-hook 'structured-haskell-mode)
-  :config
-  (progn
-    (when (require 'shm-case-split nil 'noerror)
-      ;;TODO: Find some better bindings for case-splits
-      (define-key shm-map (kbd "C-c S") 'shm/case-split)
-      (define-key shm-map (kbd "C-c C-s") 'shm/do-case-split))
-
-    (define-key shm-map (kbd "C-j") nil)
-    (define-key shm-map (kbd "C-k") nil)))
+(use-package intero
+  :config (progn
+	    (add-hook 'haskell-mode-hook 'intero-mode)))
 
 (provide 'sunra-haskell)
 
