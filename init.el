328a9dbdfcd3630326efed3643a1250a79c3faff
Timothy Washington <tim.washington@fundingcircle.com>
2016-10-06 12:35:41 -0700

    Troubleshooting mode-line foreground color

--- init.el
+++ init.el
@@ -113,6 +113,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(helm-selection ((t (:background "#191919" :distant-foreground "black"))))
- ;; '(mode-line-inactive ((t (:foreground "grey"))))
  '(which-func ((t (:foreground "#6a9fb5")))))
 
