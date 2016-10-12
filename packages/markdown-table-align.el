f634a33afbdcb84bce6fd0c436d32c78ed449a23
Timothy Washington <tim.washington@fundingcircle.com>
2016-10-12 10:41:25 -0700

    Adding a function to align tables in Markdown mode

---
+++ packages/markdown-table-align.el
@@ -0,0 +1,13 @@
+(defun *-markdown-table-align (beg end)
+  (interactive (list (org-table-begin) (org-table-end)))
+  (save-excursion    
+    (align-regexp beg end "\\(\\)|" 1 0 t)    
+    (goto-char beg)
+    (re-search-forward "|---" end t)    
+    (subst-char-in-region (line-beginning-position)
+                          (line-end-position)
+                          ?\s ?- t)))
+
+(define-prefix-command 'markdown-table-prefix-map)
+(define-key markdown-mode-map (kbd "C-c C-t") 'markdown-table-prefix)
+(define-key markdown-table-prefix-map (kbd "l") #'*-markdown-table-align)
