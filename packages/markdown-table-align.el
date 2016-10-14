4cf04fa9a664bf6206d86b88e7215cd49cb7a176
Timothy Washington <tim.washington@fundingcircle.com>
2016-10-14 01:09:32 -0700

    Adding some docs to *-markdown-table-align

--- packages/markdown-table-align.el
+++ packages/markdown-table-align.el
@@ -1,3 +1,6 @@
+
+;; this functionality generously donated by Xiongtx: https://gist.github.com/xiongtx/0dc27ef54c477aba1dde88d9fb0731bb
+
 (defun *-markdown-table-align (beg end)
   (interactive (list (org-table-begin) (org-table-end)))
   (save-excursion    
