d5e45b02c4017538d79fdfed00b7200c569c514d
Timothy Washington <twashing@gmail.com>
2025-04-18 18:39:54 -0400

    Updating Vertico to add display and selection customizations

--- packages/sunra-completion.el
+++ packages/sunra-completion.el
@@ -11,9 +11,47 @@
   :straight (vertico :type git
                      :host github
                      :repo "minad/vertico"
-                     :branch "main")
-  :init
-  (vertico-mode))
+                     :branch "main"
+                     ;; :files (:defaults "extensions/*.el")
+                     )
+
+  :init (vertico-mode)(progn
+
+          (vertico-mode)
+          (require 'vertico-buffer)
+          (require 'vertico-reverse)
+          (require 'vertico-unobtrusive)
+          (require 'vertico-mouse)
+          (require 'vertico-multiform)
+          (require 'vertico-directory)
+
+          (vertico-multiform-mode))
+
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+
+  ;; Tidy shadowed file names
+  ;; :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+
+  :config
+
+  ;; Configure multiform per category
+  (setq vertico-multiform-categories
+        '((imenu buffer mouse)
+          ;; (consult-imenu buffer mouse)
+          (t reverse mouse)))
+
+  ;; Configure multiform per command
+  (setq vertico-multiform-commands
+        '(("^describe-*" unobtrusive)
+          ;; (execute-extended-command unobtrusive)
+          ;; ("^consult-.*" buffer)
+          ;; ("^embark-.*" reverse)
+          ))
+  )
+
 
 (provide 'sunra-completion)
 ;;; sunra-completion.el ends here
