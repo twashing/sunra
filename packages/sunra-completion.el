66549a7261e68fec1d65bbd0ff6ea328cd086ba9
Timothy Washington <twashing@gmail.com>
2025-04-18 19:06:35 -0400

    Adding marginalia package

--- packages/sunra-completion.el
+++ packages/sunra-completion.el
@@ -7,13 +7,12 @@
 ;;; Code:
 
 (use-package vertico
+
   :ensure t
   :straight (vertico :type git
                      :host github
                      :repo "minad/vertico"
-                     :branch "main"
-                     ;; :files (:defaults "extensions/*.el")
-                     )
+                     :branch "main")
 
   :init (progn
           (vertico-mode)
@@ -31,11 +30,13 @@
               ("DEL" . vertico-directory-delete-char)
               ("M-DEL" . vertico-directory-delete-word))
 
+  ;; TODO - What is this
   ;; Tidy shadowed file names
   ;; :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
 
   :config
 
+  ;; TODO - After consult, embark
   ;; Configure multiform per category
   (setq vertico-multiform-categories
         '((imenu buffer mouse)
@@ -49,6 +50,19 @@
           ;; ("^consult-.*" buffer)
           ;; ("^embark-.*" reverse)
           )))
+
+
+(use-package marginalia
+
+  :ensure t
+
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  :init (marginalia-mode))
 
 
 (provide 'sunra-completion)
