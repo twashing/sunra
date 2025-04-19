19693756615475b98bdd7bf14b2c2f0dc557c420
Timothy Washington <twashing@gmail.com>
2025-04-19 13:27:36 -0400

    Update configuration for the magit commit message buffer so as not to automatically break a commit message to a newline once it hits the default column boundary

--- packages/sunra-vc.el
+++ packages/sunra-vc.el
@@ -8,6 +8,8 @@
   :commands (magit-status magit-file-dispatch)
   :bind (("C-x RET" . magit-status))
   :custom
-  (magit-no-confirm '(stage-all-changes)))
+  (magit-no-confirm '(stage-all-changes))
+  (git-commit-fill-column 1000)
+  (git-commit-summary-max-length 1000))
 
 (provide 'sunra-vc)
