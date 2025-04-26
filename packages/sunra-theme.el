78de8526600e4ac8b40f2388e8b4a574eac68752
Timothy Washington <twashing@gmail.com>
2025-04-26 19:57:08 -0400

    Minimizing vertical separator width

--- packages/sunra-theme.el
+++ packages/sunra-theme.el
@@ -52,39 +52,70 @@
 ;; https://github.com/rougier/nano-theme                           ;; Nano theme + modeline
 ;; https://github.com/rougier/nano-modeline
 
+;; (use-package doom-themes
+;; 
+;;   :ensure t
+;; 
+;;   :config
+;; 
+;;   ;; Global settings (defaults)
+;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;; 
+;;   ;; (load-theme 'doom-one t)
+;;   ;; (load-theme 'doom-one-light t)
+;;   (load-theme 'doom-flatwhite t)
+;; 
+;; 
+;; 
+;;   ;; Enable flashing mode-line on errors
+;;   (doom-themes-visual-bell-config)
+;; 
+;;   ;; ;; Enable custom neotree theme (nerd-icons must be installed!)
+;;   ;; (doom-themes-neotree-config)
+;;   ;;
+;;   ;; ;; or for treemacs users
+;;   ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+;;   ;; (doom-themes-treemacs-config)
+;; 
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config))
+
 (use-package doom-themes
 
   :ensure t
 
-  :config
+  :custom
+  (doom-themes-padded-modeline nil) ; Remove padding from modeline
 
+  :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
+        doom-themes-enable-italic t)  ; if nil, italics is universally disabled
+  
   ;; (load-theme 'doom-one t)
   ;; (load-theme 'doom-one-light t)
   (load-theme 'doom-flatwhite t)
-
-
-
+  
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-
-  ;; ;; Enable custom neotree theme (nerd-icons must be installed!)
-  ;; (doom-themes-neotree-config)
-  ;;
-  ;; ;; or for treemacs users
-  ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  ;; (doom-themes-treemacs-config)
-
+  
   ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (doom-themes-org-config)
+  
+  ;; Customize window dividers
+  (setq window-divider-default-right-width 1
+  (setq window-divider-default-places 'right-only)
+  (window-divider-mode 1)
+  
+  ;; Make vertical window dividers thinner and less visible
+  (custom-set-faces
+   '(vertical-border ((t (:foreground "#DDDDDD" :width condensed))))))
 
 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
 
- 
+
 (provide 'sunra-theme)
