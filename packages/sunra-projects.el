5b8099cdf9667af98b745112975aa444af6bd94d
Timothy Washington <twashing@gmail.com>
2025-04-28 22:19:28 -0400

    Troubleshooting getting ripgrep working for projectile

--- packages/sunra-projects.el
+++ packages/sunra-projects.el
@@ -7,12 +7,16 @@
                         :repo "bbatsov/projectile"
                         :branch "master")
 
-  :init (projectile-mode +1)
+  :init (progn
+          (projectile-mode +1)
+          (when (executable-find "rg")
+            (setq projectile-ripgrep-executable "rg")))
 
   :bind (("C-c p" . projectile-command-map))
   
   :config
   (setq projectile-completion-system 'default)
-  (setq projectile-project-search-path '(\"~/Projects\")))
+  (setq projectile-project-search-path '( "~/Projects" ))
+  (setq projectile-ripgrep-executable "rg"))
 
 (provide 'sunra-projects)
