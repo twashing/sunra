811eccc59d1b23d6b510ac72dbf7af9dabbeb387
Timothy Washington <twashing@gmail.com>
2025-05-01 21:33:49 -0400

    Ensuring git-gutter mode is enabled

--- packages/sunra-vc.el
+++ packages/sunra-vc.el
@@ -21,15 +21,17 @@
 ;;
 ;; The case of git-gutter, the modus-themes, and Doom Emacs
 ;; https://protesilaos.com/codelog/2022-08-04-doom-git-gutter-modus-themes/
-;; 
+;;
 ;; Git Gutter in Emacs
 ;; https://ianyepan.github.io/posts/emacs-git-gutter/
 (use-package git-gutter
+  :ensure t
   :hook (prog-mode . git-gutter-mode)
   :config
   (setq git-gutter:update-interval 0.02))
 
 (use-package git-gutter-fringe
+  :ensure t
   :config
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
