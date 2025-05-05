417e3f82127fdf727dad726458367ff9e480f6ce
Timothy Washington <twashing@gmail.com>
2025-05-05 19:24:42 -0400

    Code cleanup

--- packages/sunra-completion.el
+++ packages/sunra-completion.el
@@ -477,12 +477,14 @@
   :init
 
   ;; NOTE
+  ;;
   ;; Add to the global default value of `completion-at-point-functions' which is
   ;; used by `completion-at-point'.  The order of the functions matters, the
   ;; first function returning a result wins.  Note that the list of buffer-local
   ;; completion functions takes precedence over the global list.
 
   ;; NOTE
+  ;;
   ;; (sp-delete-) ; completion works here
   ;; sp-delete-   ; completion doesn't give fn options,
                   ;;  but completes the variable 'sp-delete-blank-sexps
